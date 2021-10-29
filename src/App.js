ea90fbd7997c526d4b2e59b5ae420bc8eab703ac
Jane Doe <77185646+gojanedoe@users.noreply.github.com>
2021-10-29 12:36:53 -0500

    Setup files

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+// WORKING FROM: https://youtu.be/lTjQjWemKgE?t=138
+
 import logo from "./logo.svg";
+import ComponentA from "./components/ComponentA";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <ComponentA />
+    </div>
+  );
 }
 
 export default App;
